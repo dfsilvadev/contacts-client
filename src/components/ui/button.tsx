42ca255aca46393fd28bc90c10cce86b82cbf837
Daniel Silva <dfsilva.dxp@gmail.com>
2025-05-09 13:29:08 -0300

    style: adjust the appearance of the button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,22 +9,22 @@ import {
 import { cn } from "@/lib/utils/cn";
 
 const buttonVariants = cva(
-  "flex  w-full items-center justify-center rounded-sm  px-3 py-1.5 text-sm/4 font-medium  focus-visible:outline-2 focus-visible:outline-offset-2 ",
+  "flex gap-2 w-full items-center justify-center rounded-sm  px-3 py-1.5 text-sm/4 focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer disabled:bg-gray-900 disabled:cursor-not-allowed disabled:opacity-90 disabled:text-gray-700",
   {
     variants: {
       variant: {
         default:
-          "bg-violet-800 text-slate-50 hover:bg-violet-800/90 focus-visible:outline-violet-900",
+          "bg-violet-800 text-slate-50 hover:bg-violet-800/90 focus-visible:outline-violet-900 ",
         destructive: "bg-red-600  hover:bg-red-600/90",
         outline: "border border-gray-800 bg-transparent  hover:bg-violet-800 ",
         ghost: "hover:bg-violet-800 ",
         link: "text-secondary underline-offset-4 hover:underline",
+        icon: "border border-gray-800 bg-transparent  hover:bg-violet-800 w-12",
       },
       size: {
         default: "h-12",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-14 rounded-md px-8",
-        icon: "h-10 w-10",
+        sm: "h-9 px-3 text-xs",
+        lg: "h-14 px-8",
       },
     },
     defaultVariants: {
